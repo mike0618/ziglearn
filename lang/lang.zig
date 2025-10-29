7256f7dac83b464586e70ba8dabebaaccc88b6dd
Mike Zubko <mishau7@gmail.com>
2025-10-29 16:57:52 -0600

    add payload captures

--- lang/lang.zig
+++ lang/lang.zig
@@ -755,3 +755,88 @@ test "**" {
     const memory = pattern ** 3;
     try expect(eql(u8, &memory, &[_]u8{ 0xCC, 0xAA, 0xCC, 0xAA, 0xCC, 0xAA }));
 }
+//
+// Payload Captures - uses |value| to capture value from smth.
+//
+test "optional-if" {
+    const maybe_num: ?usize = 10;
+    if (maybe_num) |n| {
+        try expect(@TypeOf(n) == usize);
+        try expect(n == 10);
+    } else {
+        unreachable;
+    }
+}
+// with if statement and err union. Err capture is required here
+test "error union if" {
+    const ent_num: error{UnknownEntity}!u32 = 5;
+    if (ent_num) |entity| {
+        try expect(@TypeOf(entity) == u32);
+        try expect(entity == 5);
+    } else |err| {
+        _ = err catch {};
+        unreachable;
+    }
+}
+// with while loops and optionals, this may have an else block.
+test "while optional" {
+    const sequence = [_]?u8{ 0xFF, 0xCC, 0x00, null };
+    var i: usize = 0;
+    while (sequence[i]) |num| : (i += 1) {
+        try expect(@TypeOf(num) == u8);
+    }
+    try expect(i == 3);
+    try expect(sequence[i] == null);
+}
+// while loops and err unions. Else with err capture is required here
+var numbers_left2: u32 = undefined;
+fn eventuallyErrorSequence() !u32 {
+    return if (numbers_left2 == 0) error.ReachedZero else blk: {
+        numbers_left2 -= 1;
+        break :blk numbers_left2;
+    };
+}
+test "while error union capture" {
+    var sum: u32 = 0;
+    numbers_left2 = 3;
+    while (eventuallyErrorSequence()) |value| {
+        sum += value;
+    } else |err| {
+        try expect(err == error.ReachedZero);
+        try expect(sum == 3);
+    }
+}
+// For loops
+test "for capture" {
+    const x = [_]i8{ 1, 5, 120, -5 };
+    for (x) |v| try expect(@TypeOf(v) == i8);
+}
+// Switch cases on tagged unions.
+const Info = union(enum) {
+    a: u32,
+    b: []const u8,
+    c,
+    d: u32,
+};
+test "switch capture" {
+    const b = Info{ .a = 10 };
+    const x = switch (b) {
+        .b => |str| blk: {
+            try expect(@TypeOf(str) == []const u8);
+            break :blk 1;
+        },
+        .c => 2,
+        // if these are of the same type, they may be inside the same capture group.
+        .a, .d => |num| blk: {
+            try expect(@TypeOf(num) == u32);
+            break :blk num * 2;
+        },
+    };
+    try expect(x == 20);
+}
+// captured with |val| are IMMUTABLE like fn args, use ptr to modify them.
+test "for with pointer capture" {
+    var data = [_]u8{ 1, 2, 3 };
+    for (&data) |*byte| byte.* += 1;
+    try expect(eql(u8, &data, &[_]u8{ 2, 3, 4 }));
+}
