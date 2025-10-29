09ecb70ff7e3dada60a060d2f17956144eb27afb
Mike Zubko <mishau7@gmail.com>
2025-10-29 08:50:48 -0600

    add enums

--- lang/lang.zig
+++ lang/lang.zig
@@ -343,3 +343,49 @@ test "slices" {
     const slice_to_end = array[0..];
     _ = slice_to_end;
 }
+// Enums - types with a restricted set of names
+const Direction = enum { north, south, east, west };
+const Value = enum(u2) { zero, one, two }; // with an int tag
+test "enum ordinal value" {
+    try expect(@intFromEnum(Value.zero) == 0); // ordinal starts at 0
+    try expect(@intFromEnum(Value.one) == 1);
+    try expect(@intFromEnum(Value.two) == 2);
+}
+const Value2 = enum(u32) {
+    hundred = 100, // val overwritten
+    thousand = 1000,
+    million = 1000000,
+    next,
+};
+test "set enum ordinal value" {
+    try expect(@intFromEnum(Value2.hundred) == 100);
+    try expect(@intFromEnum(Value2.thousand) == 1000);
+    try expect(@intFromEnum(Value2.million) == 1000000);
+    try expect(@intFromEnum(Value2.next) == 1000001);
+}
+// Enum given methods, act as namespaced fn, called with dot syntax.
+const Suit = enum {
+    clubs,
+    spades,
+    diamonds,
+    hearts,
+    pub fn isClubs(self: Suit) bool {
+        return self == Suit.clubs;
+    }
+};
+test "enum method" {
+    try expect(Suit.clubs.isClubs() == Suit.isClubs(.clubs)); // true/true
+    try expect(Suit.spades.isClubs() == Suit.isClubs(.hearts)); // false/false
+    try expect(Suit.spades.isClubs() != Suit.isClubs(.clubs)); // false/true
+}
+// Enum given var and const, act as namespaced globals, unrelated, unattached to instances of the enum type.
+const Mode = enum {
+    var count: u32 = 0;
+    on,
+    off,
+};
+test "hmm" {
+    Mode.count += 1;
+    try expect(Mode.count == 1);
+}
+// Structs
