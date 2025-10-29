9d9572b4383e4d41d955e8fc5ed061270e5221b2
Mike Zubko <mishau7@gmail.com>
2025-10-29 10:40:20 -0600

    add floats

--- lang/lang.zig
+++ lang/lang.zig
@@ -514,3 +514,33 @@ test "well defined overflow" {
     a +%= 1;
     try expect(a == 0);
 }
+//
+// Floats - @setFloatMode(.Optimized) equivalent to GCC -ffast-math
+// Coerces to larger float types.
+//
+test "float widening" {
+    const a: f16 = 0;
+    const b: f32 = a;
+    const c: f64 = b;
+    try expect(c == @as(f128, a));
+}
+// kinds of literals
+const floating_point: f64 = 123.0E+77;
+const another_float: f64 = 123.0;
+const yet_another: f64 = 123.0e+77;
+
+const hex_floating_point: f64 = 0x103.70p-5;
+const another_hex_float: f64 = 0x103.70;
+const yet_another_hex_float: f64 = 0x103.70P-5;
+// with underscore separators
+const lightspeed: f64 = 299_792_458.000_000;
+const nanosecond: f64 = 0.000_000_001;
+const more_hex: f64 = 0x1234_5678.9ABC_CDEFp-10;
+// conv functions: @floatFromInt is safe, @intFromFloat must fit dest type
+test "int-float conv" {
+    const a: i32 = 500;
+    const b = @as(f32, @floatFromInt(a));
+    // const c = @as(i8, @intFromFloat(b)); // will cause error
+    const c = @as(i32, @intFromFloat(b));
+    try expect(c == a);
+}
